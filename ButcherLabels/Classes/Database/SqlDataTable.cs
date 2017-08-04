8a315cfa15f9ddef6c879a3de3f0cf60809c2dbe
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2017-08-04 13:37:28 +0100

    Add class SqlDatatable

---
+++ ButcherLabels/Classes/Database/SqlDataTable.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ButcherLabels.Classes.Database
+{
+    public class SqlDataTable
+    {
+        public SqlConnection _sqlConnection { get; private set; }
+
+        public DataTable GetDatatable(SqlConnection sqlConn, string sqlQuery)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlQuery,sqlConn);
+
+            dataAdapter.Fill(dt);
+            return dt;
+        }
+    }
+}
